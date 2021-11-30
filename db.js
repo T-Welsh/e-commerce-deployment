292827ea778cdca60c8d56f9332a3fd90c672b28
T-Welsh <you@example.comsalthook@gmail.com>
2021-11-30 16:45:44 +0000

    Updated SSL config in db Pool

--- db.js
+++ db.js
@@ -10,7 +10,10 @@ const devConfig = {
 }
 
 const productionConfig = {
-    connectionString: process.env.DATABASE_URL //Comes from heroku addon
+    connectionString: process.env.DATABASE_URL, //Comes from heroku addon
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
 
 const pool = new Pool(process.env.NODE_ENV === "production" ? productionConfig : devConfig);
